71a143e846103f3265d40fb4896296597daa8fc8
Maxie Hernandez <maxinehernandez@hotmail.com>
2016-08-13 13:52:49 -0400

    Added associations through a migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20160813172659) do
 
-ActiveRecord::Schema.define(version: 20160813165431) do
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "patient_id"
+    t.integer  "provider_id"
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
+    t.index ["provider_id"], name: "index_appointments_on_provider_id"
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string   "email"
@@ -22,7 +34,6 @@ ActiveRecord::Schema.define(version: 20160813165431) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
-
 
   create_table "providers", force: :cascade do |t|
     t.string   "email"
