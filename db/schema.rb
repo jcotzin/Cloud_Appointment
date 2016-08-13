657fadb9082936d1a4f10820d667963c4539dfc2
karen-lam <hikarenlam@gmail.com>
2016-08-13 14:36:40 -0400

    commit schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160813163549) do
+
+  create_table "patients", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "phone"
+    t.string   "ssn"
+    t.string   "code"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
 end
