411a00b459c362dd4f1477ffe17e7e532b2bc07b
Jessica Cotzin <jcotzin@gmail.com>
2016-08-13 17:37:54 -0400

    merged schemas

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160813181250) do
+ActiveRecord::Schema.define(version: 20160813193522) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "start_time"
